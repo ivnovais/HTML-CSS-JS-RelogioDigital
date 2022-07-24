bfa7dfb4c0e1af835c8b834ffa5229d2cd81fce5
Ivandro Novais <ivandronovais@gmail.com>
2022-07-24 18:22:42 -0300

    criadas variaveis para armazenar hora local

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,9 @@
 const horas = document.getElementById("horas");
 const minutos = document.getElementById("minutos");
 const segundos = document.getElementById("segundos");
+const relogio = setInterval(function time() {
+  let dateToday = new Date();
+  let hr = dateToday.getHours();
+  let min = dateToday.getMinutes();
+  let sec = dateToday.getSeconds();
+});
