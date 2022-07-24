cbf0ecaa36d03648313e3dd91780fa6786511deb
Ivandro Novais <ivandronovais@gmail.com>
2022-07-24 18:28:03 -0300

    resolvido bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,15 @@ const relogio = setInterval(function time() {
   let hr = dateToday.getHours();
   let min = dateToday.getMinutes();
   let sec = dateToday.getSeconds();
+  if (hr < 10) {
+    hr = "0" + hr;
+  }
+  if (min < 10) {
+    min = "0" + min;
+  }
+  if (sec < 10) {
+    sec = "0" + sec;
+  }
   horas.textContent = hr;
   segundos.textContent = sec;
   minutos.textContent = min;
